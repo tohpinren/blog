da05c02965dbcb3c3055814c5e42039b2a5a5534
Pin Ren <pinren01@gmail.com>
2022-12-22 21:59:27 +0800

    Show single article

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
