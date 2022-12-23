db4165773297158e965ac61d867e1d87ccf67776
Pin Ren <pinren01@gmail.com>
2022-12-23 15:41:17 +0800

    Added basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "tpr", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
